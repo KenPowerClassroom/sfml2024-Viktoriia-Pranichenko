99cb63796048955bad8ed0787391db6762f84a01
Viktoriia-Pranichenko <c00313194@setu.ie>
2025-11-23 14:46:42 +0000

    refactor: remove duplicate code

--- 16_SFML_Games/arkanoid.cpp
+++ 16_SFML_Games/arkanoid.cpp
@@ -65,11 +65,11 @@ int arkanoid()
     int blockCount=0;
     for (int column=1; column <= MAX_BLOCK_COLUMNS; column++)
     for (int row=1;row<= MAX_BLOCK_ROWS;row++)
-      {
+    {
          block[blockCount].setTexture(blockTexture);
          block[blockCount].setPosition(column* BLOCK_WIDTH,row* BLOCK_HEIGHT);
          blockCount++;
-      }
+    }
 
     Ball ball(BALL_START_POSITION_X, BALL_START_POSITION_Y, BALL_VELOCITY_X, BALL_VELOCITY_Y);
 
@@ -106,18 +106,22 @@ void handleInput(RenderWindow& window, Sprite& paddleSprite)
 void checkBallCollisions(Ball& ball, Sprite blocks[], int blockCount, Sprite& paddleSprite)
 {
     for (int blockIndex = 0; blockIndex < blockCount; blockIndex++)
-        if (FloatRect(ball.getPositionX() + BALL_COLLISION_PADDING, ball.getPositionY() + BALL_COLLISION_PADDING, BALL_COLLISION_SIZE, BALL_COLLISION_SIZE).intersects(blocks[blockIndex].getGlobalBounds()))
-        {
-            blocks[blockIndex].setPosition(BLOCK_INACTIVATE_X, BLOCK_INACTIVATE_Y);
-			ball.bounceHorizontally();
-        }
+    {
+		FloatRect blockBounds = blocks[blockIndex].getGlobalBounds();
+        FloatRect ballBoundsX(ball.getPositionX() + BALL_COLLISION_PADDING, ball.getPositionY(), BALL_COLLISION_SIZE, BALL_COLLISION_SIZE);
+		FloatRect ballBoundsY(ball.getPositionX(), ball.getPositionY() + BALL_COLLISION_PADDING, BALL_COLLISION_SIZE, BALL_COLLISION_SIZE);
 
-    for (int blockIndex = 0; blockIndex < blockCount; blockIndex++)
-        if (FloatRect(ball.getPositionX() + BALL_COLLISION_PADDING, ball.getPositionY() + BALL_COLLISION_PADDING, BALL_COLLISION_SIZE, BALL_COLLISION_SIZE).intersects(blocks[blockIndex].getGlobalBounds()))
+        if (ballBoundsX.intersects(blockBounds))
         {
             blocks[blockIndex].setPosition(BLOCK_INACTIVATE_X, BLOCK_INACTIVATE_Y);
-            ball.bounceVertically();
+            ball.bounceHorizontally();
         }
+        if (ballBoundsY.intersects(blockBounds))
+        {
+                blocks[blockIndex].setPosition(BLOCK_INACTIVATE_X, BLOCK_INACTIVATE_Y);
+                ball.bounceVertically();
+        }
+    }
 
     if (ball.getPositionX()<0 || ball.getPositionX() >WINDOW_WIDTH)  ball.bounceHorizontally();
     if (ball.getPositionY() <0 || ball.getPositionY() >WINDOW_HEIGHT)   ball.bounceVertically();
